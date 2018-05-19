1b63d8414c712b11775e0b141f71b76f9bf231c9
hirura <hirura@gmail.com>
2018-05-19 21:23:57 +0900

    fix spec to pass all tests

--- spec/first_repo_spec.rb
+++ spec/first_repo_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe FirstRepo do
   it "has a version number" do
     expect(FirstRepo::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
